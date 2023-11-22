effc48b46dc163dfa4789d64a496df8b176161c1
James Cleveland <jc@blit.cc>
2023-11-22 12:36:20 +0000

    use a loop

--- hack/clone.sh
+++ hack/clone.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 set -e
-if [ -d "configuration-service" ]; then
-  echo "configuration-service exists"
-else
-  echo "Cloning configuration-service"
-  git clone git@github.com:compliance-framework/configuration-service.git
-fi
-if [ -d "assessment-runtime" ]; then
-  echo "assessment-runtime exists"
-else
-  echo "Cloning assessment-runtime"
-  git clone git@github.com:compliance-framework/assessment-runtime.git
-fi
-if [ -d "portal" ]; then
-  echo "portal exists"
-else
-  echo "Cloning portal"
-  git clone git@github.com:compliance-framework/portal.git
-fi
+
+for repo in "configuration-service" "assessment-runtime" "portal"; do
+  if [ -d "$repo" ]; then
+    echo "$repo exists"
+  else
+    echo "Cloning $repo"
+    git clone git@github.com:compliance-framework/configuration-service.git
+  fi
+done
 
 branch=$CONFIG_SERVICE_BRANCH
 if [ -z "$branch" ]; then branch="main"; fi
