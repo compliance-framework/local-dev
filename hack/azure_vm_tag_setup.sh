d6d703b4300cfcd4783f15ab2524458b72eeef6d
ian <ian.miell@container-solutions.com>
2024-07-22 20:14:24 +0100

    latest

--- hack/azure_vm_tag_setup.sh
+++ hack/azure_vm_tag_setup.sh
@@ -30,29 +30,10 @@ do
 done
 echo "Plan ID: ${PLANID}"
 TASKID=$(cfctl create task -p $PLANID -d "Check all the VMs for dataclassification tag" -t "VM Check" -s "0 * * * * *" | jq -r '.id')
-#TMPFILE=`mktemp` || exit 1
-#envsubst < azure_vm_tag_setup_activity.yaml > $TMPFILE
-#cat $TMPFILE
-#ACTIVITYID=$(cfctl create activity -f $TMPFILE -p $PLANID -t $TASKID | jq -r '.id')
-ACTIVITYID="$(curl -s localhost:8080/api/plan/"${PLANID}"/tasks/"${TASKID}"/activities --header 'Content-Type: application/json' -d '
-{
-  "title":"CheckVMs for dataclassification tag",
-  "description":"This activity checks for the existence of a datalassification tag",
-  "provider":{
-    "name":"azure-cf-plugin",
-    "tag":"seed",
-    "image":"ghcr.io/compliance-framework/azure-cf-plugin",
-    "configuration":{
-      "yaml": "subscriptionid: '${AZURE_SUBSCRIPTION_ID}'\ntenantid: '${AZURE_TENANT_ID}'\nclientid: '${AZURE_CLIENT_ID}'"
-    }
-  },
-  "subjects":{
-    "title":"VMs",
-    "description":"All VMs",
-    "labels":{}
-  }
-}
-' | jq -r .id)"
+TMPFILE=`mktemp` || exit 1
+envsubst < azure_vm_tag_setup_activity.yaml > $TMPFILE
+cat $TMPFILE
+ACTIVITYID=$(cfctl create activity -f $TMPFILE -p $PLANID -t $TASKID | jq -r '.id')
 echo $ACTIVITYID
 cfctl activate plan $PLANID
 rm -f $TMPFILE
